9910d8e066b8ea191ef66af2efbc2874c4b31f8f
Manh Trong Kien <manhtrongkien1901@gmail.com>
2024-10-08 16:59:19 +0700

    Upload all file

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+  ],
+  theme: {
+    extend: {
+      spacing: {
+        106: "106px",
+        84: "84px",
+        448: "448px",
+        554: "554px",
+        342: "342px",
+        732: "732px",
+        330: "330px",
+        692: "692px"
+      }
+    },
+  },
+  plugins: [],
+}
+
